1e15dda648ed634e56821da5d136360f3df8bf61
rafael <fco.rafael.santos@gmail.com>
2019-12-12 17:25:08 -0300

    Alterações importantes

--- alunos/new.php
+++ alunos/new.php
@@ -2,103 +2,27 @@
 
         <div class="content">   
             <div class="title">
-                Novo Responsável Financeiro
+                Novo Aluno
             </div>         
             <form id="form" method="post" action="save.php">            
+                <?php
+                    if(isset($_GET['responsavel_id']) and $_GET['responsavel_id'] != '') {
+                ?>                
+                    <input type="hidden" name="responsavel_id" value="<?php echo $_GET['responsavel_id']; ?>" />
+                <?php
+                        $sql = "SELECT * FROM alunos where responsavel_id = ".$_GET['responsavel_id'];
+                    } else
+                        echo "<script>window.location.href='../responsaveis/index.php';</script>";
+                ?>                
+
                 <label><span>Nome: </span><input type="text" name="nome"/></label>
-                <label><span>Nacionalidade: </span><input type="text" name="nacionalidade"/></label>
-                <label><span>Data de Nascimento: </span><input class="date" type="text" name="nascimento"/></label>
-                <label><span>RG: </span><input type="text" name="rg"/></label>
-                <label><span>CPF: </span><input type="text" class="cpf" name="cpf"/></label>
-                <label><span>Grau de Relacionamento: </span><input type="text" name="relacionamento"/></label>
-                <label><span>E-mail: </span><input type="text" class="email" name="email"/></label>
-                <label><span>Endereço: </span><input type="text" name="endereco"/></label>
-                <label><span>CEP: </span><input type="text" class="cep" name="cep"/></label>
-                <label><span>Telefone: </span><input type="text" name="telefone"/></label>
-                <label>
-                    <span>Unidade: </span>
-                    <select name="unidade">
-                        <option value='null'>Selecione...</option>
-                        <!-- <option>FB Júnior Sul</option>
-                        <option>FB Júnior Eusébio</option> -->
-                        <?php
-                            $sql = "SELECT * FROM unidade";
-                            if($result = mysqli_query($connect, $sql)){
-                                if(mysqli_num_rows($result) > 0){
-                                    while($row = mysqli_fetch_array($result)){
-                                        echo "<option value='".$row['id']."'>";
-                                            echo $row['descricao'];
-                                        echo "</option>";
-                                    }
-
-                                    // Free result set
-                                    mysqli_free_result($result);
-                                } else{
-                                    echo "";
-                                }
-                            }
-                        ?>
-                    </select>
-                </label>
-                
-                <div class="alunos">
-                    <div class="session">ALUNOS (AS)</div>
-
-                    <div class="aluno">
-                        <label><span>Nome: </span><input type="text" name="nome_aluno[]"/></label>
-                        <label><span>Série/FB2 ou Regular: </span><input type="text" name="serie_aluno[]"/></label>
-                        <label><span>Data de Nascimento: </span><input type="text" name="nascimento_aluno[]"/></label>
-                        <label><span>Restrição Alimentar: </span><input type="text" name="restricao_aluno[]"/></label>
-                    </div>
-                </div>                
-                <button type="button" class="more aluno">Adicionar</button>
-
-                <div class="session">PLANO CONTRATADO</div>
-                <label><span>Pacote: </span><textarea name="pacote"/></textarea></label>
-                <label><span>Valor Total: </span><input type="text" class="money2" name="valor"/></label>
-                <label><span>Parcelamento: </span><input type="text" name="parcelamento"/></label>
-                <label>
-                    <span>Forma de Pagamento: </span>
-                    <select name="pagamento">
-                        <option value=''>Selecione...</option>
-                        <option value="VISTA">A vista</option>
-                        <option value="BOLETO">Boleto</option>
-                        <option value="CHEQUE">Cheque</option>
-                    </select>
-                </label>
-                <label><span>Observações: </span><textarea name="observacoes"/></textarea></label>
-
-                <div class="session">CONTRATO</div>
-                <label>
-                    <span>Modelo: </span>
-                    <select name="contrato">
-                        <option value='null'>Selecione...</option>
-                        <!-- <option>2020</option>
-                        <option>2020 (2 ALUNOS)</option>
-                        <option>2020 (3 ALUNOS)</option> -->
-                        <?php
-                            $sql = "SELECT * FROM contratos";
-                            if($result = mysqli_query($connect, $sql)){
-                                if(mysqli_num_rows($result) > 0){
-                                    while($row = mysqli_fetch_array($result)){
-                                        echo "<option value='".$row['id']."'>";
-                                            echo $row['descricao'];
-                                        echo "</option>";
-                                    }
-
-                                    // Free result set
-                                    mysqli_free_result($result);
-                                } else{
-                                    echo "";
-                                }
-                            }
-                        ?>
-                    </select>
-                </label>
+                <label><span>Série/FB2 ou Regular: </span><input type="text" name="serie"/></label>
+                <label><span>Data de Nascimento: </span><input type="text" name="nascimento"/></label>
+                <label><span>Restrição Alimentar: </span><input type="text" name="restricao"/></label>
                 
                 <div class="actions">
-                    <button type="button" onclick="submitForm(index.php?status=1);">Gravar</button>
-                    <button type="button" class="secondary" onclick="window.location='list.php';">Cancelar</button>
+                    <button type="button" onclick="submitForm('index.php?status=1&responsavel_id=<?php echo $_GET['responsavel_id']; ?>');">Gravar</button>
+                    <button type="button" class="secondary" onclick="window.location='index.php?responsavel_id=<?php echo $_GET['responsavel_id']; ?>';">Cancelar</button>
                 </div>
             </form>
         </div>
--- unidades/index.php
+++ unidades/index.php
@@ -1,17 +1,20 @@
 <?php include '../header.php'; ?>
 
         <script>  
+            function editar(id) {
+                window.location.href = 'edit-responsavel.php?id='+id;
+            }
+
             function remover(id) {
-                popup("Deseja excluir o aluno?",
+                popup("Deseja excluir o reponsável financeiro?",
                     [
                         {"name": "Sim", "action": function(){
                             $.get( "remove.php?id="+id)
                               .done(function( data ) {
                                 if(data == '') {
-                                    console.log(data);
-                                    window.location.href= 'index.php?status=2&page='+page+'&responsavel_id=<?php echo $_GET['responsavel_id']; ?>';
+                                    window.location.href= 'index.php?status=2&page='+page;
                                 } else {
-                                    
+                                    console.log(data);
                                     alert('Não foi possível realizar a operação.');                    
                                 }
                               });
@@ -32,36 +35,41 @@
 
         <div class="content" style="padding-top: 70px;">  
             <div class="title">
-                Alunos
+                Responsáveis
                 
-                <a class="button" href="new.php?responsavel_id=<?php echo $_GET['responsavel_id'];?>">Novo Aluno</a>
+                <a class="button" href="new.php">Novo Responsável</a>
             </div>
             
             <?php
                 if (isset($_GET['status']) && $_GET['status'] == '1' )
-                    echo '<div class="msg">Aluno inserido com sucesso! <i class="fa fa-close" onclick="$(\'.msg\').remove();"></i></div>';
+                    echo '<div class="msg">Responsável financeiro inserido com sucesso! <i class="fa fa-close" onclick="$(\'.msg\').remove();"></i></div>';
 
                 if (isset($_GET['status']) && $_GET['status'] == '2' )
-                    echo '<div class="msg">Aluno excluído com sucesso! <i class="fa fa-close" onclick="$(\'.msg\').remove();"></i></div>';
+                    echo '<div class="msg">Responsável financeiro excluído com sucesso! <i class="fa fa-close" onclick="$(\'.msg\').remove();"></i></div>';
 
                 if (isset($_GET['status']) && $_GET['status'] == '3' )
-                    echo '<div class="msg">Aluno atualizado com sucesso! <i class="fa fa-close" onclick="$(\'.msg\').remove();"></i></div>';
+                    echo '<div class="msg">Responsável financeiro atualizado com sucesso! <i class="fa fa-close" onclick="$(\'.msg\').remove();"></i></div>';
             ?>
 
             <?php
-                if(isset($_GET['responsavel_id']) and $_GET['responsavel_id'] != '')
-                    $sql = "SELECT * FROM alunos where responsavel_id = ".$_GET['responsavel_id'];
-                else
-                    echo "<script>window.location.href='../responsaveis/index.php';</script>";
+                $sql = "SELECT r.nome as nome, r.id as id, u.descricao as unidade FROM responsaveis r left join unidade u on u.id = r.unidade_id";
+                
+                $sql = $sql.' WHERE 1=1';
+                if(isset($_GET['search']))
+                    $sql = $sql." and r.nome like '%".$_GET['search']."%'";
+
+                if(isset($_GET['unidade']) and $_GET['unidade'] != '')
+                    $sql = $sql." and u.id = ".$_GET['unidade'];
 
                 if($result = mysqli_query($connect, $sql)){
-                    
                     if(mysqli_num_rows($result) > 0){
+                        
             ?>
             <table>
                 <thead>
                     <tr>
-                        <th>Aluno</th>              
+                        <th>Responsável</th>
+                        <th width="150px">Unidade</th>                 
                         <th width="85px">Ações</th>
                     </tr>
                 </thead>
@@ -74,8 +82,12 @@
                             <div class="nome"><?php echo $row['nome']; ?></div>                            
                         </td>
                         <td>
+                            <?php echo $row['unidade']; ?>
+                        </td>
+                        <td>
                             <div class="actions">
                                      <i title="Editar" onclick="window.location.href='edit.php?id=<?php echo $row['id']; ?>';" class="fa fa-pencil" aria-hidden="true"></i>
+                                     <i title="Contratos" class="fa fa-files-o" aria-hidden="true"></i>
                                      <i title="Remover" onclick="remover(<?php echo $row['id']; ?>)" class="fa fa-trash remove" aria-hidden="true"></i>
                             </div>
                         </td>
@@ -90,12 +102,12 @@
             </table>
             <div id="pagination"></div>
             <?php
-
                     } else {
-                            echo ' <div class="no-results"><i class="fa fa-search"></i> Não existem alunos para esse responsável listados.</div>';
+                            echo ' <div class="no-results"><i class="fa fa-search"></i> Não existem responsáveis a serem listados.</div>';
                     }
                 }                   
             ?>
 
         </div>
+
 <?php include '../footer.php'; ?>
